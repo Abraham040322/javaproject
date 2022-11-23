5c6dc9a680162c79afa0b98f07bdee11a6c5454e
TOSHIBA <TOSHIBA@SH>
2022-11-22 19:31:54 -0700

    carga incial

---
+++ src/ejemplo_githup_1/Ejemplo_GitHup_1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejemplo_githup_1;
+
+/**
+ *
+ * @author TOSHIBA
+ */
+public class Ejemplo_GitHup_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            System.out.println("abraham");
+
+    }
+    
+}
